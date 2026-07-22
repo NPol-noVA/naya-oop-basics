84629186341015fc7dc47f10d68a7a5d1839252f
NPol-noVA <2025007553@aurak.ac.ae>
2026-07-22 09:56:35 +0400

    Update main-library.py
    change main

--- main-library.py
+++ main-library.py
@@ -18,8 +18,28 @@ class Library:
             print(f"Author: {k.author}")
             print(f"Title: {k.year}")
 			print()
+	
 
-          	
+    def borrow_book (self, title):
+        k = 0
+        for j in self.books:
+            if j.title == title and j.is_borrowed == False:
+                print ("This book is available and it is successfully borrowed")
+               
+				j.is_borrowed = True
+                k = 1
+        if k == 0:
+            print ("This book isn't available now")
+	def returning_book(self, title):
+        k = 0  
+        for j in self.books:
+            if j.title == title and j.is_borrowed :
+                j.is_borrowed = False
+                k = 1 
+        
+		 if k ==0 : 
+            print ("This book isn't borrowed from this library ") 
+                  	
 library = Library()
 while True: 
     print("what is your book (0 to finish)") 
@@ -35,3 +55,10 @@ while True:
     library.add_book(book)
     print ('successful operation')
 library.show_all() 
+print ("if you want to borrow a book enter 1")
+x = int (input())
+if x == 1:
+	s = input("Book Title:").lower()
+	borrow_book(s)
+
+    
